0c25aad235827a2162e954794ce5aba1a8dab4b6
Eduardo Vinimic <edu.vns.mkm@gmail.com>
2014-04-03 04:59:51 -0300

    Adicionado os Models e os Controllers do Album

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -4,22 +4,39 @@ namespace Album\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class AlbumController extends \Zend\Mvc\Controller\AbstractActionController {
+class AlbumController extends \Zend\Mvc\Controller\AbstractActionController
+{
+    protected $albumTable;
+
+
+    public function indexAction()
+    {
+        return new ViewModel(array(
+           'albums' => $this->getAlbumTable()->fetchAll(),
+        ));
+    }
     
-    public function indexAction() {
+    public function addAction()
+    {
         
     }
     
-    public function addAction() {
+    public function editAction()
+    {
         
     }
     
-    public function editAction() {
+    public function deleteAction()
+    {
         
     }
     
-    public function deleteAction() {
-        
+    public function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
     }
-    
 }
